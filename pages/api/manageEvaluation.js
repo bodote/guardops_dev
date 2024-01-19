2efb9d3ad5fc8d3f0b0464b6a5ce3ebc983c15b5
ajay <abd.bodara@gmail.com>
2024-01-19 18:35:18 +0530

    changes added

---
+++ pages/api/manageEvaluation.js
@@ -0,0 +1,38 @@
+import { getToken } from "@/utils/getToken";
+
+export default async function handler(req, res) {
+  let Url = null;
+  let queryParams = null;
+  let urlWithParams = null;
+  let bodyData = null;
+  const baseUrl = process.env.BackendBaseUrl;
+
+  const { method } = req;
+
+  const token = await getToken();
+
+  switch (method) {
+    case "GET":
+      Url = `${baseUrl}api/get_evaluation_list`;
+      queryParams = new URLSearchParams({
+        project_id: req.query.project_id,
+      });
+      urlWithParams = `${Url}?${queryParams}`;
+
+      try {
+        const response = await fetch(urlWithParams, {
+          method: "GET",
+          headers: new Headers({
+            authorization: `Bearer ${token}`,
+          }),
+        });
+
+        const data = await response.json();
+        res.status(response.status).json(data);
+      } catch (error) {
+        console.error("Error during API request:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+      break;
+  }
+}
