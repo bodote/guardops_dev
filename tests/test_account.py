e6dfbf656cb67e13235e50a25817b62cdf81a765
f.maag <f.maag@hs-ansbach.de>
2024-03-15 14:40:18 +0100

    switched to clipboard instead of pyperclip

--- tests/test_account.py
+++ tests/test_account.py
@@ -8,12 +8,13 @@ from selenium.webdriver.chrome.options import Options
 from test_login import test_login
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import clipboard
 class AccountTests(unittest.TestCase):
 
     def setUp(self):
-        options = Options()
-        options.add_argument('--headless=new')
-        self.driver = webdriver.Chrome(options=options)
+        #options = Options()
+        #options.add_argument('--headless=new')
+        self.driver = webdriver.Chrome()
 
     def test_datasets(self):
         driver = self.driver
@@ -34,7 +35,8 @@ def test_keys(driver: webdriver.Chrome):
     WebDriverWait(driver,10).until(EC.presence_of_element_located(locator=(By.XPATH,"//*[contains(text(),'Copy key to clipboard')]")))
     time.sleep(0.2)
     driver.find_element(By.XPATH, value="//*[contains(text(),'Copy key to clipboard')]").click()
-    clipboard_content = pyperclip.paste()
+    time.sleep(0.2)
+    clipboard_content = clipboard.paste()
     assert "coai" in clipboard_content and len(clipboard_content)==50, "copying of new key failed"
 
 if __name__ == "__main__":
