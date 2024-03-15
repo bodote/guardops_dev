10322b4fd9e0519689f0aa50155c743b67fe6167
f.maag <f.maag@hs-ansbach.de>
2024-03-15 14:49:16 +0100

    cl

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import unittest
@@ -8,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 from test_login import test_login
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import clipboard
 class AccountTests(unittest.TestCase):
 
     def setUp(self):
@@ -36,7 +34,7 @@ def test_keys(driver: webdriver.Chrome):
     time.sleep(0.2)
     driver.find_element(By.XPATH, value="//*[contains(text(),'Copy key to clipboard')]").click()
     time.sleep(0.2)
-    clipboard_content = clipboard.paste()
+    clipboard_content = pyperclip.paste()
     assert "coai" in clipboard_content and len(clipboard_content)==50, "copying of new key failed"
 
 if __name__ == "__main__":
