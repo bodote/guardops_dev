6b0af5a82868009f9631f67d8252350d07dfb9df
f.maag <f.maag@hs-ansbach.de>
2024-03-18 15:32:35 +0100

    typo

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -26,7 +26,7 @@ class DatasetTests(unittest.TestCase):
 def test_dataset_creation(driver: webdriver.Chrome):
     driver.get("http://localhost:3000/datasetlist")
     WebDriverWait(driver,10).until(EC.presence_of_element_located(locator=(By.XPATH,"//*[contains(text(),'New Dataset')]")))
-    driver.find_element(By.XPATH, value="//*[contains(text(),'New Project')]").click()
+    driver.find_element(By.XPATH, value="//*[contains(text(),'New Dataset')]").click()
     driver.find_element(By.NAME, value="project_name").send_keys("Testing Dataset")
     driver.find_element(By.NAME, value="project_description").send_keys("Description")
     driver.find_element(By.XPATH, value="//*[contains(text(),'Save Dataset')]").click()
