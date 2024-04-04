8e2f382aae48924135f7e3f7dca92aba5afe9b54
f.maag <f.maag@hs-ansbach.de>
2024-04-04 15:01:31 +0200

    project selection sorting

--- app/playground/page.js
+++ app/playground/page.js
@@ -470,7 +470,17 @@ const index = () => {
       .replace(/[^\w\s]/g, "")
       .replace(/\s+/g, "");
     return trimmedProjectName.match(regex);
-  });
+  }) .sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0; // names must be equal
+  });;
   // State to manage versions
   const [versions, setVersions] = useState([
     { id: 1, component: <Version key={1} /> },
