96a84b6e956ee3ac5f3fcd51118c61ae13e819df
f.maag <f.maag@hs-ansbach.de>
2024-06-19 19:55:04 +0200

    removed debug prints

--- utils/getToken.js
+++ utils/getToken.js
@@ -4,23 +4,18 @@ const redis = Redis.fromEnv();
 export const getToken = async () => {
   const redisToken = "access_token";
   try {
-    console.log("Getting token")
     const cachedToken = await redis?.get(redisToken);
 
     if (cachedToken) {
-      console.log("This token is cached",cachedToken);
       return cachedToken;
     }
-    console.log("Getting a new token");
     const response = await fetch(
       `${process.env.AUTH0_BASE_URL}/api/auth/token`
     );
     const data = await response.json();
-    console.log("This is my response data", data);
     const newToken = data.access_token;
 
     if (newToken) {
-      console.log("Got this new token", newToken);
       await redis.set(redisToken, newToken, { ex: 86400 });
       return newToken;
     } else {
