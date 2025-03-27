8b9aa37ca73def273936d9d60a0d8c92a71e2512
fmaag <f.maag@hs-ansbach.de>
2025-03-27 11:54:06 +0100

    chroma https package error fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 const nextConfig = {
   output: 'standalone',
 
@@ -10,21 +9,22 @@ const nextConfig = {
     serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
     missingSuspenseWithCSRBailout: false,
 
-},
+  },
 
-webpack: (config) => {
-  // Ignore node-specific modules when bundling for the browser
-  // See https://webpack.js.org/configuration/resolve/#resolvealias
-  config.resolve.alias = {
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
       ...config.resolve.alias,
       "sharp$": false,
       "onnxruntime-node$": false,
-  }
-  config.externals.push({
-    'https://unpkg.com/@xenova/transformers@2.13.2': 'transformers',
-  });
-  return config;
-},
+    }
+    config.externals.push({
+      'https://unpkg.com/@xenova/transformers@2.13.2': 'transformers',
+      'https://unpkg.com/chromadb-default-embed@2.14.0': 'chromadb-default-embed',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
